31874cab0ea08536654fc66fa1f03d1e427ea578
laurent <laurent.canet@sogelink.fr>
2013-05-26 17:26:19 +0200

    some tests

--- reader-server/test/rest1.js
+++ reader-server/test/rest1.js
@@ -5,8 +5,11 @@ var server = require('../server'),
 
 describe('Rest API Test', function(){
 
+    var backendUrl = 'http://localhost:8080';
+
+
     it('Should have feeds', function(done){
-        request('http://localhost:8080')
+        request(backendUrl)
             .get('/feed')
             .expect(200)
             .expect('Content-Type', 'application/json; charset=utf-8')
@@ -28,7 +31,7 @@ describe('Rest API Test', function(){
 
 
     it('Should have 1 feed', function(done){
-        request('http://localhost:8080')
+        request(backendUrl)
             .get('/feed/1134')
             .expect(200)
             .expect('Content-Type', 'application/json; charset=utf-8')
@@ -45,7 +48,7 @@ describe('Rest API Test', function(){
 
 
     it('Should have 1 icon ', function(done){
-        request('http://localhost:8080')
+        request(backendUrl)
             .get('/feed/1134/icon')
             .expect(200)
             .expect('Content-Type', 'image/png')
@@ -56,12 +59,43 @@ describe('Rest API Test', function(){
 
 
     it('Should not have an unknown feed', function(done){
-        request('http://localhost:8080')
+        request(backendUrl)
             .get('/feed/11343010')
             .expect(404)
             .end(function(err,res){
                 done();
             });
     });
+
+
+    it('Should update feed poll date', function(done){
+        var setDate = new Date();
+        request(backendUrl)
+            .put('/feed/1134')
+            .type('application/json')
+            .send({last_poll: setDate.getTime()})
+            .expect(200)
+            .end(function(err,res){
+                if (!err) {
+                    // one more request to check
+                    request(backendUrl)
+                        .get('/feed/1134')
+                        .expect(200)
+                        .expect('Content-Type', /json/)
+                        .end(function(err2, res2){
+                            var feed = res2.body;
+                            feed.should.have.property('id', 1134);
+                            feed.should.have.property('last_poll', setDate.toJSON());
+
+                            done();
+                        });
+                } else {
+                    assert.fail('Feed update failed');
+                    done();
+                }
+            });
+    });
+
+
 
 });
