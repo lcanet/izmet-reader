04d7ba01cb1e54fbcbd5b039729b2cd9f5898774
lct <lc@lc6.net>
2013-08-11 19:43:20 +0200

    grunt syntax

--- web-client/Gruntfile.js
+++ web-client/Gruntfile.js
@@ -341,7 +341,7 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('build', [
-         'clean:dist',
+        'clean:dist',
         'jshint',
         'test',
         'coffee',
