14d827e8e21ac137d10b632dd6a3269bd31d3c01
lct <lc@lc6.net>
2013-11-02 18:16:53 +0100

    Added AWS monitoring shell scripts (to be cronned)

---
+++ monitoring/aws/monitor_fetch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DB_HOST=localhost
+DB_USER=reader
+DN_NAME=reader
+
+CW_ACCESS=YOUR_CLOUDWATCh_ACCESS_KEY
+CW_SECRET=YOUR_CLOUDWATCh_ACCESS_SECRET
+CW_REGION="us-west-2"
+
+DATFMT=$(date +'%Y-%m-%dT%H:%M:%S.000%z')
+NB=$(echo "select count(1) from article where (now() - fetch_date) < interval '1 hour' " | psql -h $DB_HOST -U $DB_USER  -A -t  $DB_NAME)
+
+mon-put-data --metric-name NbFetch1H --namespace Izmet --value $NB --timestamp $DATFMT -u Count -I $CW_ACCESS -S $CW_SECRET --region $CW_REGION
+
+
